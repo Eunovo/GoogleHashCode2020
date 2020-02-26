5ae2bd27e4a07a839fc96a269338eed6903fed06
Eunovo <eunovo9@gmail.com>
2020-02-26 22:56:26 +0100

    Handle signup process in simulation

--- solution.py
+++ solution.py
@@ -13,11 +13,11 @@ def main():
     global input_files
     global output_dir
 
-    for file in input_files:
+    for file in input_files[0:1]:
         problem = parse_input(file)
         print("Parsed ", file)
-        # problem_solver = ProblemSolver(problem)
-        # (score, solution) = problem_solver.solve()
+        problem_solver = ProblemSolver(problem)
+        problem_solver.solve()
         # output_file = output_dir + "/" \
         #     + file.split("/")[-1].split(".")[0] + ".out"
         # write_output(solution, output_file)
@@ -48,12 +48,13 @@ def parse_input(file):
         lib_index = i + start_index
         libr_descr = get_tokens(lines[lib_index])
         lib_id = i
-        lib_n_books = libr_descr[0]
-        sign_up_time = libr_descr[1]
-        books_per_day = libr_descr[2]
+        lib_n_books = int(libr_descr[0])
+        sign_up_time = int(libr_descr[1])
+        books_per_day = int(libr_descr[2])
 
         lib_books = get_tokens(lines[lib_index + 1])
-        libraries.append(Library(lib_id, lib_books, books_per_day))
+        libraries.append(Library(
+            lib_id, sign_up_time, lib_books, books_per_day))
 
         i += 2
 
@@ -69,11 +70,48 @@ def write_output(solution, output_file):
 
 class ProblemSolver:
     def __init__(self, problem):
-        pass
+        self.problem = problem
+        self.currently_signing = -1
+        self.last_signed = -1
+        self.started_on = None
 
     def solve(self):
-        pass
+        sorted_libraries = sorted(self.problem.libraries, key=self.sort_key)
+        # print([l.sign_up_time for l in sorted_libraries])
+        signed_libraries = []
+        day_index = 0
+        while(day_index < self.problem.n_days):
+            new_lib = self.handle_signup(day_index, sorted_libraries)
+            if (new_lib != None):
+                signed_libraries.append(new_lib)
+            
 
+
+            day_index += 1
+
+    def sort_key(self, library):
+        return library.sign_up_time
+
+    def handle_signup(self, day, libraries):
+        def start_next():
+            next_index = self.last_signed + 1
+            if (next_index >= len(libraries)):
+                return
+            self.currently_signing = next_index
+            self.started_on = day
+
+        if (self.currently_signing == -1):
+            start_next()
+            return None
+
+        current_lib = libraries[self.currently_signing]
+        if (day - self.started_on == current_lib.sign_up_time):
+            self.last_signed = self.currently_signing
+            self.currently_signing = -1
+            start_next()
+            return current_lib
+        return None
+        
 class Problem:
     def __init__(self, libraries, books, n_days):
         self.libraries = libraries
@@ -81,8 +119,9 @@ class Problem:
         self.n_days = n_days
 
 class Library:
-    def __init__(self, l_id, books, books_per_day):
+    def __init__(self, l_id, sign_up_time, books, books_per_day):
         self.l_id = l_id
+        self.sign_up_time = sign_up_time
         self.books = books
         self.books_per_day = books_per_day
         
